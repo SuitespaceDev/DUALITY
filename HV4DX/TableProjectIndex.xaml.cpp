fdeff4f89da0cbb0b5e60c896ba7726d0bc1a7ac
Rebeka Fox <rebekafox@outlook.com>
2023-01-13 01:51:49 -0800

    01/13/2023 01:51

---
+++ HV4DX/TableProjectIndex.xaml.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "TableProjectIndex.xaml.h"
+#if __has_include("TableProjectIndex.g.cpp")
+#include "TableProjectIndex.g.cpp"
+#endif
+
+namespace winrt::HV4DX::implementation
+{
+    TableProjectIndex::TableProjectIndex()
+    {
+        InitializeComponent();
+
+        return;
+
+    }
+
+}
---
+++ HV4DX/TableProjectIndex.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+
+#include "TableProjectIndex.g.h"
+
+namespace winrt::HV4DX::implementation
+{
+    struct TableProjectIndex : TableProjectIndexT<TableProjectIndex>
+    {
+    public:
+        TableProjectIndex();
+
+    };
+
+}
+
+namespace winrt::HV4DX::factory_implementation
+{
+    struct TableProjectIndex : TableProjectIndexT<TableProjectIndex, implementation::TableProjectIndex>
+    {
+    };
+}
