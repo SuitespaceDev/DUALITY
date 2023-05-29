1590791518626c254a080f7d0a613e8343577ff7
Rebeka Fox <rebekafox@outlook.com>
2023-05-28 21:16:02 -0700

    05/28/2023 21:15

--- HV4D/HV4DCOBJECT.cpp
+++ HV4D/HV4DCOBJECT.cpp
@@ -50,9 +50,96 @@ namespace winrt::HV4DCOBJECT::implementation
 
 	}
 
+	winrt::Windows::Foundation::Collections::IVector<HV4D::HV4DT_TYPE_CONTRACT> HV4DCOBJECT::HV4DSupportedInterfaces()
+	{
+
+
+		return SupportedInterfaces;
+
+	}
+
 	void HV4DCOBJECT::HV4DListSupportedInterfaces()
 	{
 		SupportedInterfaces = winrt::single_threaded_vector<HV4D::HV4DT_TYPE_CONTRACT>();
+
+
+
+	}
+
+	winrt::guid HV4DCOBJECT::HV4DUuidOfInstance()
+	{
+
+
+
+	}
+
+	uint64_t HV4DCOBJECT::HV4DCreatedDateTime()
+	{
+
+
+
+	}
+
+	uint64_t HV4DCOBJECT::HV4DUpdatedDateTime()
+	{
+
+
+
+	}
+
+	winrt::guid HV4DCOBJECT::HV4DUuidOfClassObject() 
+	{ 
+	
+
+		return winrt::guid{L"{839C965B-45FC-475A-A163-57A83D712293}"}; 
+	
+	}
+
+	winrt::hstring HV4DCOBJECT::HV4DNameOfClassObject()
+	{
+
+
+
+	}
+
+	winrt::guid HV4DCOBJECT::HV4DUuidOfCollection() 
+	{ 
+		
+		
+		return winrt::guid{L"{05EAF3E1-F4E5-458B-BD95-A7121C22826A}"}; 
+	
+	}
+
+	void HV4DCOBJECT::HV4DSetUuidOfInstance()
+	{
+
+
+
+	}
+
+	void HV4DCOBJECT::HV4DSetCreatedDateTime()
+	{
+
+
+
+	}
+
+	void HV4DCOBJECT::HV4DSetUpdatedDateTime()
+	{
+
+
+
+	}
+
+	HV4D::IHV4DTTYPE HV4DCOBJECT::HV4DGetTType()
+	{
+
+
+
+	}
+
+	HV4D::IHV4DSERVICE HV4DCOBJECT::HV4DGetService()
+	{
 
 
 
--- HV4D/HV4DCOBJECT.h
+++ HV4D/HV4DCOBJECT.h
@@ -10,7 +10,7 @@ namespace WF = winrt::Windows::Foundation;
 namespace WFC = winrt::Windows::Foundation::Collections;
 namespace HV4D = winrt::HV4D;
 
-namespace winrt::HV4DCOBJECT::implementation
+namespace winrt::HV4D::implementation
 {
 	struct HV4DCOBJECT : HV4DCOBJECTT<HV4DCOBJECT>
 	{
@@ -22,42 +22,50 @@ namespace winrt::HV4DCOBJECT::implementation
 		HV4DCOBJECT(HV4D::IHV4DCOBJECT const&, winrt::guid const&, WF::IInspectable const&);
 
 	private:
-		void HV4DListSupportedInterfaces();
+		HV4D::IHV4DCOBJECT owner_handle{ nullptr };
+
+		winrt::guid extended_args_type{ L"{2D048E34-FDC5-4230-9C3A-BD05DAF60D1B}" };
+
+		WF::IInspectable extended_args{ nullptr };
 
 	public:
-		winrt::Windows::Foundation::Collections::IVector<HV4D::HV4DT_TYPE_CONTRACT> HV4DSupportedInterfaces() 
-		{
-			return SupportedInterfaces;
-
-		};
+		winrt::Windows::Foundation::Collections::IVector<HV4D::HV4DT_TYPE_CONTRACT> HV4DSupportedInterfaces();
 
 	private:
+		void HV4DListSupportedInterfaces();
+
 		winrt::Windows::Foundation::Collections::IVector<HV4D::HV4DT_TYPE_CONTRACT> SupportedInterfaces{};
 
 	public:
 		winrt::guid HV4DUuidOfInstance();
 
-		winrt::guid HV4DUuidOfClassObject() { return L"{839C965B-45FC-475A-A163-57A83D712293}"; };
-
-		winrt::hstring HV4DClassObjectName();
-
-		winrt::guid HV4DUuidOfCollection() { return L"{05EAF3E1-F4E5-458B-BD95-A7121C22826A}"; };
-
 		uint64_t HV4DCreatedDateTime();
 
 		uint64_t HV4DUpdatedDateTime();
 
+		winrt::guid HV4DUuidOfClassObject();
+
+		winrt::hstring HV4DNameOfClassObject();
+
+		winrt::guid HV4DUuidOfCollection();
+
 	private:
+		void HV4DSetUuidOfInstance();
+
 		winrt::guid uuid_of_instance{};
 
+		void HV4DSetCreatedDateTime();
+
 		uint64_t date_created{};
+
+		void HV4DSetUpdatedDateTime();
 
 		uint64_t date_updated{};
 
 	public:
-		HV4D::IHV4DTTYPE HV4DGetTType() { return HV4D::HV4DT_TYPE_CONTRACT{}; };
+		HV4D::IHV4DTTYPE HV4DGetTType();
 
-		HV4D::IHV4DSERVICE HV4DGetService() { return; };
+		HV4D::IHV4DSERVICE HV4DGetService();
 
 
 
@@ -70,7 +78,7 @@ namespace winrt::HV4DCOBJECT::implementation
 
 }
 
-namespace winrt::HV4DCOBJECT::factory_implementation
+namespace winrt::HV4D::factory_implementation
 {
 	struct HV4DCOBJECT : HV4DCOBJECTT<HV4DCOBJECT, implementation::HV4DCOBJECT>
 	{
